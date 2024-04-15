623ee20cbd6f8b8345d921f60f87dc7914614c12
tklingstrom <tomas.klingstrom@slu.se>
2024-04-15 09:41:54 +0000

    Small script to plot frequency of responses by heard.

--- Kok scripts/calvingstatistics.R
+++ Kok scripts/calvingstatistics.R
@@ -3,12 +3,13 @@
 #install.packages("dplyr")
 #install.packages("dbplyr")
 #install.packages("ggplot2")
-
+#install.packages("likert")
 
 library(odbc)
 library(DBI)
 library(dplyr)
 library(dbplyr)
+library(likert)
 library(ggplot2)
 
 
@@ -22,10 +23,10 @@ con <- dbConnect(odbc(),
                  TrustServerCertificate = "yes")
 odbcListObjects(con)
 
-Calving_Con = con %>% tbl(in_catalog("gigacow_qa", "sciKok", "Kok_Calving"))
+Calving_Con = con %>% tbl(in_catalog("gigacow", "sciKok", "Kok_Calving"))
 
 #The below code chunks run SQL queries on each table and then downloads the result.
-Calving_Con = con %>% tbl(in_catalog("gigacow_qa", "sciKok", "Kok_Calving")) %>%
+Calving_Con = con %>% tbl(in_catalog("gigacow", "sciKok", "Kok_Calving"))
 Calving_Cow_Raw = collect(Calving_Con)
 
 #This is a test to make sure that the number of calvings remain the same after the recoding is done.
@@ -33,9 +34,9 @@ calving_counts_Raw <- Calving_Cow_Raw %>%
   group_by(CalvingEase) %>%
   summarise(count = n())
 
-#Recoding to numerical values
+#Step: 1: Recoding to numerical values and storing levels
 Calving_Cow <- Calving_Cow_Raw %>%
-  mutate(CalvingEase = recode(CalvingEase,
+  mutate(CalvingEase = as.factor(dplyr::recode(CalvingEase,
                               'Lätt, ingen hjälp (L)' = 1,
                               'Lätt, med hjälp (L)' = 2,
                               'Normal förlossning (N)' = 3,
@@ -44,9 +45,38 @@ Calving_Cow <- Calving_Cow_Raw %>%
                               'Svår förlossning (kon bedöms ej kunnat kalva utan draghjälp) (S)' = 6,
                               'Tidig kalvning (T)' = 7,
                               'Felläge (F)' = 8,
-                              'Uppgift om förlossning saknas' = 9))
+                              'Uppgift om förlossning saknas' = 9)))
 
-#Count to make sure data remains the same
-calving_counts <- Calving_Cow %>%
+CalvingEase_levels <- c('Lätt, ingen hjälp (L)', 
+                        'Lätt, med hjälp (L)', 
+                        'Normal förlossning (N)', 
+                        'Svår, utan veterinärhjälp (S)',
+                        'Svår, med Veterinärhjälp (S)',
+                        'Svår förlossning (kon bedöms ej kunnat kalva utan draghjälp) (S)',
+                        'Tidig kalvning (T)',
+                        'Felläge (F)',
+                        'Uppgift om förlossning saknas'
+                        )
+
+calving_counts <- Calving_Cow %>% #Count to make sure data remains the same
   group_by(CalvingEase) %>%
   summarise(count = n())
+
+# Step 2: Filter data for farms with at least 50 calvings
+filtered_data <- Calving_Cow %>%
+  group_by(ActiveHerdNumber) %>%
+  filter(n() >= 50) %>%
+  ungroup()
+
+filtered_data %>%
+  group_by(ActiveHerdNumber) %>%
+  summarise(MeanCE = mean(as.integer(CalvingEase)), SD_CE = sd(as.integer(CalvingEase)), n = n())
+
+
+CalvingEaseSummary = filtered_data %>%
+  group_by(ActiveHerdNumber, CalvingEase) %>%
+  summarise(n = n()) %>%
+  mutate (freq = n / sum(n)) %>%
+  arrange(ActiveHerdNumber, CalvingEase)
+
+sum(CalvingEaseSummary$n)  
